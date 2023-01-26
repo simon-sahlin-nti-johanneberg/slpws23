7f656f33a5471a252fc3634cc0f09a30c6469894
simon-sahlin-nti-johanneberg <95085773+simon-sahlin-nti-johanneberg@users.noreply.github.com>
2023-01-26 12:26:34 +0100

    Game crud functionality

--- app.rb
+++ app.rb
@@ -28,17 +28,51 @@ get('/games/gamebruh') do
   slim(:playgame)
 end
 
-get('/games/addgame') do
-  slim(:addgame)
+get('/games/gamebruhs') do
+  slim(:gamepage)
 end
 
+get('/games/addgame') do
+  db = SQLite3::Database.new("db/database.db")
+  db.execute("INSERT INTO games (name, description, imagePath, showInList) VALUES (?,?,?,?)", "Game Title", "Game Description", "img/thumbnails/thumb-Blinded.jpg", 0)
+  redirect('/games')
+end
+
+get('/games/editgame/:id') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/database.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM games WHERE id = ?", id).first
+  slim(:addgame, locals:{result:result})
+end
+
+=begin
 post('/games/addgame') do
   title = params[:title]
   desc = params[:description]
   imgPath = params[:imagePath]
   show = !params[:display].nil? ? 1 : 0
-  p "hello #{show}"
+
   db = SQLite3::Database.new("db/database.db")
   db.execute("INSERT INTO games (name, description, imagePath, showInList) VALUES (?,?,?,?)", title, desc, imgPath, show)
-  redirect('/games/addgame')
+  redirect('/games')
+=end
+
+post('/games/editgame') do
+  id = params[:id]
+  title = params[:title]
+  desc = params[:description]
+  imgPath = params[:imagePath]
+  show = !params[:display].nil? ? 1 : 0
+
+  db = SQLite3::Database.new("db/database.db")
+  db.execute("UPDATE games SET name=?, description=?, imagePath=?, showInList=? WHERE id=?", title, desc, imgPath, show, id)
+  redirect('/games')
+end
+
+post('/games/deletegame') do
+  id = params[:id]
+  db = SQLite3::Database.new("db/database.db")
+  db.execute("DELETE FROM games WHERE id = ?", id)
+  redirect('/games')
 end
